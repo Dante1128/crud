55a01fc089dd075849f1f5adc79fb12b3e6b72c8
Dante1128 <danteyupanqui2811@gmail.com>
2025-10-07 13:18:37 -0400

    crud

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,11 @@
 <?php
-use Illuminate\Foundation\Application as LaravelApplication;
+
+use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
-$app = LaravelApplication::configure(basePath: dirname(__DIR__))
+return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -14,9 +16,5 @@ $app = LaravelApplication::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    });
-
-// FORZAR STORAGE TEMPORAL PARA VERCEL
-$app->useStoragePath(sys_get_temp_dir() . '/laravel/storage');
-
-return $app->create();
+    })
+    ->create();
